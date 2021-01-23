4c612e864fd1ced579d0eb3523b021167d403572
MorelCorpJeff <jfmorel@gmail.com>
2021-01-23 14:37:09 -0500

    first commit

---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+
+  return response.data;
+};
+
+export default LandingPage;
