0793857fc1d709bc88f6bfacaefda3315c03aba4
MorelCorpJeff <jfmorel@gmail.com>
2021-05-08 11:43:09 -0400

    updating to new cluster name

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting Auth Service......');
+  console.log('Starting Auth Service...');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
