fcde90644ffc03975f6921a7e592e17309c173e4
MorelCorpJeff <jfmorel@gmail.com>
2021-05-09 14:40:03 -0400

    Update index.js
    revert

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,43 +1,43 @@
 export default () => <p1>Hello</p1>;
 
-// import Link from 'next/link';
+import Link from 'next/link';
 
-// const LandingPage = ({ currentUser, tickets }) => {
-//   const ticketList = tickets.map((ticket) => {
-//     return (
-//       <tr key={ticket.id}>
-//         <td>{ticket.title}</td>
-//         <td>{ticket.price}</td>
-//         <td>
-//           <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
-//             <a>View</a>
-//           </Link>
-//         </td>
-//       </tr>
-//     );
-//   });
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
 
-//   return (
-//     <div>
-//       <h2>Tickets</h2>
-//       <table className="table">
-//         <thead>
-//           <tr>
-//             <th>Title</th>
-//             <th>Price</th>
-//             <th>Link</th>
-//           </tr>
-//         </thead>
-//         <tbody>{ticketList}</tbody>
-//       </table>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h2>Tickets</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
+};
 
-// LandingPage.getInitialProps = async (context, client, currentUser) => {
-//   const { data } = await client.get('/api/tickets');
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
 
-//   return { tickets: data };
-// };
+  return { tickets: data };
+};
 
-// export default LandingPage;
+export default LandingPage;
