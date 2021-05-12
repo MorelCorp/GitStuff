f280934a1535e2e242dab7564c20e849d5a59a5d
MorelCorpJeff <jfmorel@gmail.com>
2021-05-12 13:20:04 -0400

    Update build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const Named = ({ req }) => {
     console.log('Server side called');
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://www.morelcorp.org',
       headers: req.headers,
     });
   } else {
